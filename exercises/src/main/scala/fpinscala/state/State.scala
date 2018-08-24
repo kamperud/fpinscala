928d8239047d786ea461833d4b1a45f25a472634
Hanna Kamperud <hanna.kamperud@gmail.com>
2018-08-24 15:59:19 +0200

    Page 90

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -83,18 +83,45 @@ object RNG {
       (f(a, b), rng3)
     }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    def go(acc: Rand[List[A]], list: List[Rand[A]]): Rand[List[A]] = list match {
+      case x :: xs => go(map2(x, acc)(_ :: _), xs)
+      case Nil => acc
+    }
+    go(unit(Nil), fs)
+  }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def ints2(count: Int): Rand[List[Int]] = {
+    sequence(List.fill(count)(int))
+  }
+
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1)
+    }
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt)(a => if (a + (n-1) - a%n >= 0) unit(a%n) else nonNegativeLessThan(n))
+
+  def map12[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map22[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
 }
 
 case class State[S,+A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
-    ???
+    flatMap(a => State.unit(f(a)))
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
+    this.flatMap(a => sb.map(b => f(a, b)))
   def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    State(s => {
+      val (a, s2) = run(s)
+      f(a).run(s2)
+    })
+
 }
 
 sealed trait Input
@@ -105,5 +132,19 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+
+  def unit[S, A](a: A): State[S, A] =
+    State(s => (a, s))
+
+  def sequence[A, S](fs: List[State[S, A]]): State[S, List[A]] = {
+    def go(acc: State[S, List[A]], list: List[State[S, A]]): State[S, List[A]] = list match {
+      case x :: xs => go(x.map2(acc)(_ :: _), xs)
+      case Nil => acc
+    }
+    go(unit(List()), fs)
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+    State(machine => ((1,2), machine))
+
 }
